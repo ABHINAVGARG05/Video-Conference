4f2094c0b44075f32565cdee2ff3da9f549511fc
Abhinav Garg <143117260+ABHINAVGARG05@users.noreply.github.com>
2025-08-09 22:26:16 +0530

    Update page.tsx
    fix : fix logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import CallNotitfication from "@/components/CallNotification";
 import ListOnlineUsers from "@/components/ListOnlineUsers";
 import VideoCall from "@/components/VideoCall";
-import Image from "next/image";
+//import Image from "next/image";
 
 export default function Home() {
   return (
